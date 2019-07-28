60cf3bf8b81106d3762bb6edae31135557503b3c
BooeySays <dbuihien@my.bcit.ca>
2019-07-28 14:34:25 -0700

    v0.3.1

--- installer2.sh
+++ installer2.sh
@@ -10,8 +10,16 @@ if [ "$(w3m -version | grep -qe 'w3m'; echo $?)" != '0' ]; then
     sudo apt update -y && sudo apt install w3m -y
 fi
 
+function _prepdx2(){
+    wget https://raw.githubusercontent.com/BooeySays/SetupDx2/master/DX2Setup.sh
+    . ./DX2Setup.sh
+}
+
 if [ ! $DX2 ] || [ ! $DX2RC ] || [ ! $DX2BIN ]; then
-    sudo make prep;
-else
-    sudo make install;
+    _prepdx2;
 fi
+unset _prepdx2
+
+cd "$SCDIR"
+sudo make install;
+unset SCDIR
