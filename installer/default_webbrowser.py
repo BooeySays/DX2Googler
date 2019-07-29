2792128a72fd9a2a291c3c7f7225ed169a9a1643
BooeySays <dbuihien@my.bcit.ca>
2019-07-28 20:27:50 -0700

    search bar edit

---
+++ installer/default_webbrowser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+import shutil
+import os
+
+COLS, LINES = shutil.get_terminal_size()
+
+print('\033c')
+print('\033[01;38;5;87m DX2Googler - Install\033[38;5;15m: Web Browser\033[m')
+print('\033[01;38;5;196m' + '⎺' * COLS + '\033[m\n\n')
