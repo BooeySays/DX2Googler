a52c4ced0d5348341ecd6c16c5ea903b716a5637
BooeySays <dbuihien@my.bcit.ca>
2020-07-05 19:04:10 -0700

    new google logo with no box

---
+++ borderlessgoogler.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import shutil
+
+COLS, LINES = shutil.get_terminal_size()
+
+BGB = "\033[07m \033[m"
+GOOGLELOGO = "\033[01;38;5;32m Ｇ\033[38;5;160mｏ\033[38;5;215mｏ\033[38;5;32mｇ\033[38;5;34mｌ\033[38;5;160mｅ\033[38;5;00m: \033[1C\033[48;5;252m"
+BG = "\033[48;5;252m \033[m"
+
+print("\n")
+print(" " + BG * (COLS - 2))
+print(" " + BG + BGB * (COLS - 4) + BG)
+rqrq = input(" " + BG * (COLS - 2) + "\n\r\033[2A\033[2C" + "\033[48;5;252m " + GOOGLELOGO)
+print("\n\n")
+#print()
+#Ｇｏｏｇｌｅ
