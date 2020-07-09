df5b50a4520ee17f8c2dc62faf9ea12e5199e172
BooeySays <dbuihien@my.bcit.ca>
2020-07-09 16:45:29 -0700

    working version

--- install.sh
+++ install.sh
@@ -1,72 +1,80 @@
 #!/usr/bin/env bash
 
-if [ ! -d "$HOME/.DX2" ]; then
-	while true; do
-		echo -e "DX2Googler requires SetupDX2"
-		echo -e "Install it now?"
-		echo -e ""
-		read -p "[Y/n]: "
-			case $REPLY in
-				""|"y"|"Y")
-					sudo apt-get update -y
-					sudo apt-get install w3m make python3 wget -y
-					wget https://raw.githubusercontent.com/BooeySays/SetupDx2/master/Setup.py
-					read -p "[ Hit any key to set up DX2 ]" -n 1;
-					python3 ./Setup.py
-					echo -e "\nDONE !!\n"
-					if ! command -v w3m &> /dev/null; then
-						echo -e "w3m not found";
-						sudo apt-get update -y;
-						sudo apt-get install w3m -y;
-					fi
-					if ! command -v make &> /dev/null; then
-						echo -e "Make not found"
-						sudo apt-get update -y;
-						sudo apt-get install make -y;
-					fi
-					if ! command -v python3 &> /dev/null; then
-						echo -e "Python3 not found"
-						sudo apt-get update -y;
-						sudo apt-get install python3 -y;
-					fi
-				#	echo -e "\033[m\033[01;31mInstalling w3m and make\033[m:";
-				#	sudo apt-get install w3m make -y;
-					read -p "[m[01;31mFinished - Press any key to continue and install dx2googler" -n 1;
-					make;
-#					make
-					return
-					;;
-				"n"|"N")
-					echo -e "\nExiting"
-					return
-					;;
-				*)
-					echo -e "\n\n\033[00;01;31mERR\033[37m:\033[m Invalid key"
-					echo -e ""
-					echo -e "[ Press any key to try again ]"
-					read -n 1
-					;;
-			esac
+function checkapps(){
+declare -a APPSTOCHECKFOR=("w3m" "python3" "wget" "make")
+
+if $(! command -v w3m &> /dev/null) || $(! command -v python3 &> /dev/null) || $(! command -v wget &> /dev/null) || $(! command -v make &> /dev/null); then
+	echo -e "\033[00;01;31mOne or more required apps is missing \033[m- \033[00;01;31mInstalling missing apps\033[m: "
+	echo -e "In order to install the missing apps, root permission will be required / used when executing apt-get."
+	echo -e "If prompted, please enter your password in the following prompt to continue.\n"
+	sudo echo "";
+	echo -e -n "\033[00;01;36mupdating apt-get repo\033[m\t: "
+	sudo apt-get update -y &> /dev/null;
+	echo -e "\033[00;01;32mDONE\033[m\n"
+	for apps in ${APPSTOCHECKFOR[@]}; do
+		echo -e -n "\033[00;01;36mSearching for \033[00;01m$apps\033[m\t: ";
+		if ! command -v "$apps" &> /dev/null; then
+			echo -e "\033[00;01;31mNot Found\033[m";
+			echo -e -n "  \033[00;01m- \033[m\033[00;01;36mInstalling \033[00;01m$apps\033[m\t: ";
+			sudo apt-get install "$apps" -y &> /dev/null;
+			echo -e "\033[00;01;32mDONE\033[m"
+		else
+			echo -e "\033[00;01;32mFound\033[m"
+		fi
 	done
 else
-	if ! command -v w3m &> /dev/null; then
-		echo -e "w3m not found";
-		sudo apt-get update -y;
-		sudo apt-get install w3m -y;
-	fi
-	if ! command -v make &> /dev/null; then
-		echo -e "Make not found"
-		sudo apt-get update -y;
-		sudo apt-get install make -y;
-	fi
-	if ! command -v python3 &> /dev/null; then
-		echo -e "Python3 not found"
-		sudo apt-get update -y;
-		sudo apt-get install python3 -y;
-	fi
-#	echo -e "\033[m\033[01;31mInstalling w3m and make\033[m:";
-#	sudo apt-get install w3m make -y;
-	read -p "[m[01;31mFinished - Press any key to continue and install dx2googler[m" -n 1;
-	make;
+	echo -e "\033[00;01;32mAll required apps found\033[m"
 fi
+}
 
+function installdx2setup(){
+	echo -e "\033[00;01;31mSetting up DX2Setup\033[m:"
+	echo -e -n "\tDownloading setup file\t: "
+	wget https://raw.githubusercontent.com/BooeySays/SetupDx2/master/Setup.py &> /dev/null;
+	echo -e "\033[00;01;32mDONE\033[m"
+	echo -e -n "\tSetting up DX2Setup\t: "
+	python3 ./Setup.py &> /dev/null;
+	echo -e "\033[00;01;32mDONE\033[m\n"
+}
+
+
+if [ ! -d "$HOME/.DX2" ]; then
+	echo -e "DX2Googler requires SetupDX2"
+	echo -e "Install it now?"
+	echo -e ""
+	read -p "[Y/n]: "
+	case $REPLY in
+		""|"y"|"Y")
+			checkapps;
+			installdx2setup;
+			echo -e -n "\033[00;01;31mInstalling dx2googler[m\t: ";
+			make &> /dev/null;
+			echo -e "\033[00;01;32mDONE\033[m\n"
+			. ~/.dx2rc;
+			echo -e "[m[31mFINISHED[m! To start google search, hit [31m[[mCTRL[31m][m + [31m[[mk[31m][m\n";
+#			read -p "[m[01;31mFinished - Press any key to continue and install dx2googler" -n 1;
+#			make &> /dev/null;
+#			. ~/.dx2rc;
+#			echo -e "[m[31m FINISHED! To start google search, hit [CTRL] + [k]";
+			return
+			;;
+		"n"|"N")
+			echo -e "\nExiting"
+			return
+			;;
+		*)
+			echo -e "\n\n\033[00;01;31mERR\033[37m:\033[m Invalid key"
+			echo -e ""
+			echo -e "[ Press any key to try again ]"
+			read -n 1
+			;;
+	esac
+else
+	checkapps;
+	echo -e -n "\033[00;01;31mInstalling dx2googler[m\t: ";
+	make &> /dev/null;
+	echo -e "\033[00;01;32mDONE\033[m\n"
+	. ~/.dx2rc;
+	echo -e "[m[31mFINISHED[m! To start google search, hit [31m[[mCTRL[31m][m + [31m[[mk[31m][m\n";
+#	echo -e "[m[31mFINISHED! To start google search, hit [CTRL] + [k]";
+fi
---
+++ unsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf $HOME/.DX2
+rm $HOME/.dx2rc
+mv $HOME/.bashrc $HOME/.dx2bashrc
+cp $HOME/.ogbashrc $HOME/.bashrc
